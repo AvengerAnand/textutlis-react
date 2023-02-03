e858299bf32f82b9f7a82dfcf84664f0c49429dc
Anand Sharma <ak2865483@gmail.com>
2023-02-03 18:12:57 +0530

    add some basic function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types'
 
 
 export default function TextForm(props) {
-     // Declare a new state variable, which we'll call "count"
 
-     const handleClick = () =>{
+     // Declare a new state variable, which we'll call "count"
+     const UpperCase = () =>{
       // console.log('UpperCase Was Clicked'+ text);
       let newText = text.toUpperCase();
+      setText(newText);
+     }
+
+    //  Convert  to Lower Case
+    const LowerCase = () =>{
+      // console.log('UpperCase Was Clicked'+ text);
+      let newText = text.toLowerCase();
       setText(newText);
      }
 
@@ -20,13 +27,27 @@ export default function TextForm(props) {
   const [text, setText] = useState();
 
   return (
-    <div>
+    <>
+    <div className='conatiner'>
     <div className="mb-3">
         <h1>{props.heading}</h1>
         <textarea onChange={handleOnChange} className="form-control" placeholder='Enter your Text Here to Conver to Uppercase' value={text} id="exampleFormControlTextarea1" rows="8"></textarea>
     </div>
-    <button onClick={handleClick} className='btn btn-primary'>Convert to UPPERCASE</button>
+    <button onClick={UpperCase} className='btn btn-primary'>Convert to UPPERCASE</button>
+    <button onClick={LowerCase} className='btn btn-primary mx-2'>Convert to LowerCase</button>
+   
     </div>
+
+    <div className='conatiner my-3'>
+      <h1 className='text-center'>Your Text Summery</h1>
+      <p className='text-center'>
+        {text.split(" ").length}  Words, {text.length} Characters
+       </p>
+       <h1>Priview</h1>
+       <p>{text}</p>
+    </div>
+    
+    </>
   )
 }
 
@@ -36,4 +57,3 @@ TextForm.protoTypes = {
 
 
 
-const code  = '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'
